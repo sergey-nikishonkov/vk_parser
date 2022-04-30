7f0e58f52453dc8790544c76cdd6b0c971e523a9
sergey-nikishonkov <74471754+sergey-nikishonkov@users.noreply.github.com>
2022-05-01 02:23:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,31 @@ import csv
 import time
 
 
-def all_inform():
+def all_usrs():
     group_id = input('goup_id: ')
     access_token = input('token: ')
-    user = input('number_of_users: ')
-    v = 5.131
-    count = 1000
-    offset = 0
-    all_users = []
-    print('Собираю информацию')
-    while offset < int(user):
-        fields = 'bdate, city,contacts, can_write_private_message, connections, online_mobile'
-        response = requests.get('https://api.vk.com/method/groups.getMembers', params={'group_id': group_id,
-                                                                                           'fields': fields,
-                                                                                           'count': count,
-                                                                                           'offset': offset,
-                                                                                           'access_token': access_token,
-                                                                                           'v': v})
-        print(response.json())
-        data = response.json()['response']['items']
+    users_count = input('number_of_users: ')
+    params = {
+        'group_id': group_id,
+        'access_token': access_token,
+        'fields': fields,
+        'count': 1000,
+        'offset': 0,
+        'v': 5.31
+    }
+    users = []    
+    while offset < int(users_count):
         offset += 1000
-        all_users.extend(data)
-        time.sleep(0.5)
-        print('Собираю информацию')
+        all_users.extend(requests.get
+                         (
+            'https://api.vk.com/method/groups.getMembers',
+            params=params
+                         ).json()['response']['items'])
+    return users
 
-    return all_users
 
-users = all_inform()
 
 def file_writer(users):
-    print('Создаю и записываю файл')
     with open('Users.csv', 'w', newline='',encoding='utf-8') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(['Имя', 'Город', 'Дата рождения', 'Телефон', 'Skype', 'Instagram', 'Ссылка'])
@@ -48,5 +43,4 @@ def file_writer(users):
         with open('Users.csv', 'a', newline='',encoding='utf-8') as file:
             writer = csv.writer(file, delimiter=';')
             writer.writerow(inf)
-    print('Файл записан')
-file_writer(users=users)
+file_writer(all)
